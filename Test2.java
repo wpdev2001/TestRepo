80c85936c0c230eece72c345d637481ecc9a6b4b
piyush <priyanshudevilliers@gmail.com>
2025-08-04 17:40:28 +0530

    resolve the conflict occurred during commit

--- Test2.java
+++ Test2.java
@@ -3,5 +3,6 @@ public class Test2 {
         System.out.println("This first line from test2 modified by shreyash");
         System.out.println("this is second line in test2 by sk");
         System.out.println("Added third line again");
+        System.out.println("This first line from test2 modified by sk");
     }
 }
